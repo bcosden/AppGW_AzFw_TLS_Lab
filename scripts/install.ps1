26ebeca71b2725d6bb76099497f7c3dc4eb5fba4
nynex <bruce@cosden.me>
2023-07-08 21:24:18 -0400

    initial

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -1,7 +1,7 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString("https://chocolatey.org/install.ps1"))
-choco install apache-httpd --params '"/installLocation:C:\httpd"'
-choco install strawberryperl
+choco install apache-httpd -y --params '"/installLocation:C:\httpd"'
+choco install strawberryperl -y
 choco install npcap -y --version="0.86.0"
 choco install wireshark -y
 choco install fiddler -y --install-arguments '"/D=C:\fiddler"'
